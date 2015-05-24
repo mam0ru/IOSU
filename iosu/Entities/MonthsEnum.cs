651977402d993ef815a3a1f51867b57a41e23907
mam0ru <alex.truhan@mail.ru>
2015-05-24 16:22:38 +0300

    reports missed files

---
+++ iosu/Entities/MonthsEnum.cs
@@ -0,0 +1,4 @@
+﻿namespace iosu.Entities
+{
+    public enum Months : byte { Jan = 1, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec };
+}
---
+++ iosu/Models/View/SelerDynamic.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using iosu.Entities;
+
+namespace iosu.Models.View
+{
+    public class SelerDynamic
+    {
+        public String Selers { get; set; }
+
+        public Dictionary<Months, long> Months { get; set; }
+    }
+}
